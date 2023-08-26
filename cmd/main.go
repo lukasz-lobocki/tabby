f00132b575557fe80e9e95b74e3f63877277eead
lukasz-lobocki <lukasz.lobocki@googlemail.com>
2023-08-26 14:26:39 +0200

    chore: Main added

--- cmd/main.go
+++ cmd/main.go
@@ -1,148 +1,40 @@
-package tabby
+package main
 
 import (
-	"errors"
-	"fmt"
-	"regexp"
-	"strings"
-	"unicode/utf8"
+	"log"
+
+	"github.com/lukasz-lobocki/tabby/pkg/tabby"
+	"github.com/lukasz-lobocki/tabby/pkg/utils"
 )
 
-type Table struct {
-	headers []string
-	rows    [][]string
-}
+func main() {
 
-type Config struct {
-	padding string
-	spacing string
-}
+	_tab := new(tabby.Table)
 
-// Adds headers to the table
-func (_t *Table) AddHeaders(headers []string) error {
-
-	// Error if no headers provided
-	if len(headers) < 1 {
-		return errors.New(fmt.Sprint("no headers provided."))
+	if err := _tab.AddHeaders([]string{
+		"something",
+		"bnother",
+		"one mo" + utils.RED + "r" + utils.RESET + "e",
+	}); err != nil {
+		log.Fatalln(err)
 	}
 
-	*_t = Table{
-		headers: headers,
-	}
-	return nil
-}
-
-// Appends row of cells to the table
-func (_t *Table) AddRowCells(row []string) error {
-
-	// Error if number of cells in the row exceeds the number of headers
-	if len(row) > len(_t.headers) {
-		return errors.New(
-			fmt.Sprintf("number of cells %d in the row %v exceeds the number of headers %d.",
-				len(row),
-				row,
-				len(_t.headers)))
+	if err := _tab.AddRowCells([]string{
+		"uno",
+		"dos",
+		"tres",
+		//"quatro",
+	}); err != nil {
+		log.Fatalln(err)
 	}
 
-	_t.rows = append(_t.rows, row)
-	return nil
-}
-
-// Prints the table
-func (_t Table) Print(c *Config) {
-
-	if c == nil {
-		// defaultConfig returns the default config for table
-		c = getDefaultConfig()
+	if err := _tab.AddRowCells([]string{
+		"jeden",
+		"kl" + utils.RED + "m" + utils.RESET + "no67890",
+		"trzy",
+	}); err != nil {
+		log.Fatalln(err)
 	}
 
-	// Measure columns for biggest widht
-	_columnsWidth := getColumnsWidth(_t)
-
-	// Emit header
-	fmt.Println(
-		formatTableLine(
-			_t.headers,
-			_columnsWidth,
-			c.padding,
-			c.spacing))
-
-	// Iterate and emit rows
-	for _, _row := range _t.rows {
-		// Emit row
-		fmt.Println(
-			formatTableLine(
-				_row,
-				_columnsWidth,
-				c.padding,
-				c.spacing))
-	}
-	return
-}
-
-// Provides the default config for table
-func getDefaultConfig() *Config {
-	return &Config{
-		padding: " ",
-		spacing: "  ",
-	}
-}
-
-// Formats table line appending cells, padding to given width and spacing between the cells
-func formatTableLine(_l []string, _columnsWidth []int, padding string, spacing string) string {
-
-	var _ln strings.Builder
-
-	for i, _cell := range _l {
-		// Append each cell padded
-		_ln.WriteString(padRight(_cell, _columnsWidth[i], padding))
-		if i < len(_l)-1 {
-			// Append spacing but not after last column; adding +1 to adjust 0-based loop
-			_ln.WriteString(spacing)
-		}
-	}
-	return _ln.String()
-}
-
-// Returns string padded to visible rune lenght.
-func padRight(input string, lenght int, padding string) string {
-
-	// If input not shorter than lenght, return input
-	if _runeCount := getRuneCount(input); _runeCount >= lenght {
-		return input
-	}
-
-	return input + strings.Repeat(padding, lenght-getRuneCount(input))
-}
-
-// Returns longest runic lenght of each column with header.
-func getColumnsWidth(_t Table) []int {
-
-	_output := make([]int, len(_t.headers))
-
-	// Measure header
-	for i, _header := range _t.headers {
-		_output[i] = getRuneCount(_header)
-	}
-
-	// Iterate and measure rows
-	for _, _row := range _t.rows {
-		for j, _cell := range _row {
-			if _thisLength := getRuneCount(_cell); _thisLength > _output[j] {
-				_output[j] = _thisLength
-			}
-		}
-	}
-	return _output
-}
-
-// Returns string with ANSI codes removed.
-func removeANSICodes(input string) string {
-	_regexp := regexp.MustCompile(`\x1b\[[0-9;]*[mK]`)
-	return _regexp.ReplaceAllString(input, "")
-}
-
-// Returns rune count with ANSI codes removed.
-func getRuneCount(input string) int {
-	return utf8.RuneCountInString(removeANSICodes(input))
+	_tab.Print(nil)
 }
---
+++ pkg/tabby/tabby.go
@@ -0,0 +1,148 @@
+package tabby
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+	"unicode/utf8"
+)
+
+type Table struct {
+	headers []string
+	rows    [][]string
+}
+
+type Config struct {
+	padding string
+	spacing string
+}
+
+// Adds headers to the table
+func (_t *Table) AddHeaders(headers []string) error {
+
+	// Error if no headers provided
+	if len(headers) < 1 {
+		return errors.New(fmt.Sprint("no headers provided."))
+	}
+
+	*_t = Table{
+		headers: headers,
+	}
+	return nil
+}
+
+// Appends row of cells to the table
+func (_t *Table) AddRowCells(row []string) error {
+
+	// Error if number of cells in the row exceeds the number of headers
+	if len(row) > len(_t.headers) {
+		return errors.New(
+			fmt.Sprintf("number of cells %d in the row %v exceeds the number of headers %d.",
+				len(row),
+				row,
+				len(_t.headers)))
+	}
+
+	_t.rows = append(_t.rows, row)
+	return nil
+}
+
+// Prints the table
+func (_t Table) Print(c *Config) {
+
+	if c == nil {
+		// defaultConfig returns the default config for table
+		c = getDefaultConfig()
+	}
+
+	// Measure columns for biggest widht
+	_columnsWidth := getColumnsWidth(_t)
+
+	// Emit header
+	fmt.Println(
+		formatTableLine(
+			_t.headers,
+			_columnsWidth,
+			c.padding,
+			c.spacing))
+
+	// Iterate and emit rows
+	for _, _row := range _t.rows {
+		// Emit row
+		fmt.Println(
+			formatTableLine(
+				_row,
+				_columnsWidth,
+				c.padding,
+				c.spacing))
+	}
+	return
+}
+
+// Provides the default config for table
+func getDefaultConfig() *Config {
+	return &Config{
+		padding: " ",
+		spacing: "  ",
+	}
+}
+
+// Formats table line appending cells, padding to given width and spacing between the cells
+func formatTableLine(_l []string, _columnsWidth []int, padding string, spacing string) string {
+
+	var _ln strings.Builder
+
+	for i, _cell := range _l {
+		// Append each cell padded
+		_ln.WriteString(padRight(_cell, _columnsWidth[i], padding))
+		if i < len(_l)-1 {
+			// Append spacing but not after last column; adding +1 to adjust 0-based loop
+			_ln.WriteString(spacing)
+		}
+	}
+	return _ln.String()
+}
+
+// Returns string padded to visible rune lenght.
+func padRight(input string, lenght int, padding string) string {
+
+	// If input not shorter than lenght, return input
+	if _runeCount := getRuneCount(input); _runeCount >= lenght {
+		return input
+	}
+
+	return input + strings.Repeat(padding, lenght-getRuneCount(input))
+}
+
+// Returns longest runic lenght of each column with header.
+func getColumnsWidth(_t Table) []int {
+
+	_output := make([]int, len(_t.headers))
+
+	// Measure header
+	for i, _header := range _t.headers {
+		_output[i] = getRuneCount(_header)
+	}
+
+	// Iterate and measure rows
+	for _, _row := range _t.rows {
+		for j, _cell := range _row {
+			if _thisLength := getRuneCount(_cell); _thisLength > _output[j] {
+				_output[j] = _thisLength
+			}
+		}
+	}
+	return _output
+}
+
+// Returns string with ANSI codes removed.
+func removeANSICodes(input string) string {
+	_regexp := regexp.MustCompile(`\x1b\[[0-9;]*[mK]`)
+	return _regexp.ReplaceAllString(input, "")
+}
+
+// Returns rune count with ANSI codes removed.
+func getRuneCount(input string) int {
+	return utf8.RuneCountInString(removeANSICodes(input))
+}
